aee85f16c4ce2b7c4fe2924608ad929c46e38e10
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2024-03-21 10:10:32 -0300

    finish notify

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "../styles/bootstrap/css/bootstrap.css";
 import Logo from "../images/logos/lupalogo.png";
-import { Div, IMG, Span } from "../styles/loginStyle";
+import { Div, IMG, Span, H5, Notification, Div2, PathNotes, CloseBTN } from "../styles/loginStyle";
 import Message from "../components/message";
 import Loading from "../components/loading";
 import "animate.css";
 
 export default function Login() {
+  const version = "1.0";
+  const [Notify, SetNotify] = useState(true);
   useEffect(() => {
     document.title = "FixDesk";
     const Theme = localStorage.getItem("Theme");
@@ -21,6 +23,32 @@ export default function Login() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    const new_version = localStorage.getItem("version");
+    if (new_version === null) {
+      SetNotify(true);
+    } else if (new_version !== version) {
+      SetNotify(true);
+    } else if (new_version === version) {
+      SetNotify(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Notify) {
+      const nt = document.getElementById("ntf");
+
+      nt.addEventListener("animationend", () => {
+        if (nt.classList.contains("animate__fadeInUp")) {
+          nt.classList.remove("animate__fadeInUp");
+          nt.classList.remove("animate__slow");
+          nt.classList.add("animate__pulse");
+          nt.classList.add("inifit");
+        }
+      });
+    }
+  }, [Notify]);
+
   const [loginPage, SetLoginPage] = useState(true);
   const [passlimit, SetPassLimit] = useState(false);
   const [awaitValidation, SAwaitValidation] = useState(false);
@@ -30,6 +58,18 @@ export default function Login() {
   const [animate, SetAnimate] = useState("");
   const [theme, SetTheme] = useState("");
   const [color, SetColor] = useState("");
+  const [pathnotes, SetPatchNotes] = useState(false);
+
+  useEffect(() => {
+    if (pathnotes) {
+      const new_version = localStorage.getItem("version");
+      if (new_version === null) {
+        localStorage.setItem("version", version);
+      } else if (new_version !== version) {
+        localStorage.setItem("version", version);
+      }
+    }
+  }, [pathnotes]);
 
   function ThemeBlack() {
     SetColor("colorBlack");
@@ -152,6 +192,47 @@ export default function Login() {
         <div className="position-absolute top-50 start-50 translate-middle">
           <Loading />
         </div>
+      )}
+      <Div2 className="position-absolute bottom-0 start-0 d-flex">
+        <H5
+          onClick={() => {
+            SetPatchNotes(true);
+          }}
+        >
+          {version}
+        </H5>
+        a
+        {Notify && (
+          <Notification
+            onClick={() => {
+              SetPatchNotes(true);
+            }}
+            id="ntf"
+            className="animate__animated animate__fadeInUp animate__slow"
+          ></Notification>
+        )}
+      </Div2>
+      {pathnotes && (
+        <PathNotes className="d-flex flex-column position-absolute top-50 start-50 translate-middle z-3">
+          <CloseBTN
+            className="position-absolute top-0 end-0 btn btn-danger"
+            onClick={() => {
+              SetPatchNotes(false);
+            }}
+          >
+            X
+          </CloseBTN>
+          <h3 className="text-center mb-5 mt-3 fw-bold">Notas de Atualizações</h3>
+          <div className="d-flex flex-column">
+            <h6 className="fw-bold mb-2">Nota de Atualização 1.0 - 21/03</h6>
+            <p className="text-break">
+              <b>Assunto</b>: Data de Lançamento da Ferramenta de Chamados FixDesk É com grande satisfação que anunciamos a data oficial de lançamento da nossa nova ferramenta de chamados, FixDesk.
+              Após um período de desenvolvimento e testes rigorosos, estamos felizes em informar que o FixDesk estará disponível para uso a partir de [data de lançamento]. Esta ferramenta
+              revolucionária promete simplificar e otimizar o processo de gerenciamento de chamados, proporcionando uma experiência mais eficiente e organizada para toda a equipe. Fiquem atentos para
+              mais informações sobre treinamentos e orientações sobre como utilizar o FixDesk da melhor forma possível.
+            </p>
+          </div>
+        </PathNotes>
       )}
     </Div>
   );
--- frontend/src/styles/loginStyle.js
+++ frontend/src/styles/loginStyle.js
@@ -15,3 +15,38 @@ export const Span = styled.span`
   font-weight: bold;
   font-size: 1.3em;
 `;
+
+export const H5 = styled.h5`
+  color: white;
+  cursor: pointer;
+`;
+
+export const Notification = styled.div`
+  content: "";
+  width: 1em;
+  height: 1em;
+  background: #affc41;
+  margin-bottom: 5px;
+  margin-left: 5px;
+  border-radius: 50%;
+  cursor: pointer;
+  -webkit-box-shadow: 0px 0px 28px 3px rgba(175, 252, 65, 1);
+  -moz-box-shadow: 0px 0px 28px 3px rgba(175, 252, 65, 1);
+  box-shadow: 0px 0px 28px 3px rgba(175, 252, 65, 1);
+`;
+
+export const Div2 = styled.div`
+  margin: 10px;
+`;
+
+export const PathNotes = styled.div`
+  width: 60%;
+  height: 60%;
+  background-color: #eae0d5;
+  padding: 1.5em;
+  border-radius: 1em;
+`;
+
+export const CloseBTN = styled.button`
+  margin: 1em;
+`;
