0406a07e5657ff48eb92956cd4d29fd37c388b49
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2024-03-21 10:12:13 -0300

    upload code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/login";
 import Helpdesk from "./pages/helpdesk";
 import History from "./pages/history";
-import FAQ from "./pages/faq";
+import DashboardTI from "./dashboard/dashboardTI";
 
 export default function App() {
   const router = createBrowserRouter([
@@ -21,7 +21,7 @@ export default function App() {
     },
     { path: "/helpdesk", element: <Helpdesk /> },
     { path: "/helpdesk/history", element: <History /> },
-    { path: "/helpdesk/FAQ", element: <FAQ /> },
+    { path: "/dashboard_TI", element: <DashboardTI /> },
   ]);
   return <RouterProvider router={router} />;
 }
